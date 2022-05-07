dce9ed443afe5cfccc91f3b3f3198e214bcfbc0b
ShittyWizard <anisimov.develop@gmail.com>
2022-05-07 17:00:01 +0300

    solution for Middle of the linked list (876);

---
+++ src/problems/middleOfTheLinkedList_876/Solution.java
@@ -0,0 +1,36 @@
+package problems.middleOfTheLinkedList_876;
+
+// link : https://leetcode.com/problems/middle-of-the-linked-list/
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast.next != null) {
+            return slow.next;
+        } else {
+            return slow;
+        }
+    }
+}
